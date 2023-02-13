906814152f24ed31d7a5458446b2b990769ffb45
Matt Baker <baker.matt.j@gmail.com>
2023-02-13 11:16:13 -0800

    install: better communicate progress with status output.

--- install.sh
+++ install.sh
@@ -6,14 +6,16 @@ KENV="${HOME}/klippy-env"
 BKDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 if [ ! -d "$KDIR" ] || [ ! -d "$KENV" ]; then
-    echo "klipper or klippy env doesn't exist"
+    echo "beacon: klipper or klippy env doesn't exist"
     exit 1
 fi
 
 # install beacon requirements to env
+echo "beacon: installing python requirements to env, this may take 10+ minutes."
 "${KENV}/bin/pip" install -r "${BKDIR}/requirements.txt"
 
 # update link to beacon.py
+echo "beacon: linking klippy to beacon.py."
 if [ -e "${KDIR}/klippy/extras/beacon.py" ]; then
     rm "${KDIR}/klippy/extras/beacon.py"
 fi
@@ -23,3 +25,4 @@ ln -s "${BKDIR}/beacon.py" "${KDIR}/klippy/extras/beacon.py"
 if ! grep -q "klippy/extras/beacon.py" "${KDIR}/.git/info/exclude"; then
     echo "klippy/extras/beacon.py" >> "${KDIR}/.git/info/exclude"
 fi
+echo "beacon: installation successful."
